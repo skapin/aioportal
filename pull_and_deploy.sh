7e11062a6086c9e3b44e2690c51b7703296ff9ed
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-05-05 11:13:13 +0200

    force rebuild for screenshots

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -2,9 +2,8 @@
 
 
 cd /home/kombos/intranet.aio.eu/aioportal
-
 RESPULL=`git pull`
-if [[ $RESPULL != *"Already up to date"* ]]; then
-  echo "===>Rebuilding HUGO static file"
-  /home/linuxbrew/.linuxbrew/bin/hugo -D
-fi
+# if [[ $RESPULL != *"Already up to date"* ]]; then
+echo "===>Rebuilding HUGO static file"
+/home/linuxbrew/.linuxbrew/bin/hugo -D
+# fi
