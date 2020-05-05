2b2c059cc797285e7df96a57b13b550babf9380e
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-05-05 10:48:15 +0200

    pull and deploy

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -6,5 +6,5 @@ cd /home/kombos/intranet.aio.eu/aioportal
 RESPULL=`git pull`
 if [[ $RESPULL != *"Already up to date"* ]]; then
   echo "===>Rebuilding HUGO static file"
-  # /home/linuxbrew/.linuxbrew/bin/hugo -D
+  /home/linuxbrew/.linuxbrew/bin/hugo -D
 fi
