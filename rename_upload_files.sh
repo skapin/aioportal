dbdf00edf243b74a71ea80b5a64cdc5158297803
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-05-07 16:06:57 +0200

    git mv

--- rename_upload_files.sh
+++ rename_upload_files.sh
@@ -2,7 +2,7 @@
 
 
 cd /home/kombos/intranet.aio.eu/aioportal/static/
-for F in *; { mv "$F" "${F// /-}"; }
+for F in *; { git mv "$F" "${F// /-}"; }
 git add .
 git commit -m "automatic upload filename renamed"
 git push
