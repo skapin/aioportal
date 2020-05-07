c655a31afc6253d0ff9f25b28ad0f6e7696da3c8
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-05-07 15:15:23 +0200

    first try rename uploads

--- rename_upload_files.sh
+++ rename_upload_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cd /home/alexandre/intranet/aioportal/static/
+cd /home/kombos/intranet.aio.eu/aioportal/static/
 for F in *; { mv "$F" "${F// /-}"; }
 git add .
 git commit -m "automatic upload filename renamed"
