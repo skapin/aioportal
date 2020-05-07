144dbe22104e9cef751b2227b31d042ac46b72be
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-05-07 17:11:25 +0200

    cleaning

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -5,6 +5,5 @@ cd /home/kombos/intranet.aio.eu/aioportal
 RESPULL=`git pull`
 if [[ $RESPULL != *"Already up to date"* ]]; then
   echo "===>Rebuilding HUGO static file"
-  # /home/kombos/intranet.aio.eu/aioportal/rename_upload_files.sh
   /home/linuxbrew/.linuxbrew/bin/hugo -D
 fi
--- rename_upload_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cd /home/kombos/intranet.aio.eu/aioportal/static/
-for F in *; { git mv "$F" "${F// /-}"; }
-git add .
-git commit -m "automatic upload filename renamed"
-git push
